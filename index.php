ac7763b9f50cbc44bac9b713d311582345509c3e
Henriknick <deyvisson.henrik@gmail.com>
2019-09-09 09:38:38 -0300

    Landing Page

---
+++ index.php
@@ -0,0 +1,122 @@
+<!Doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto 01</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+
+<body>
+
+<header>
+    <div class="center">
+        <img src="image/Logo.png" title="Web9 Soluções">
+    </div>
+    <!--header-->
+</header>
+<div class="container-banner">
+    <div class="center">
+        <div class="container-list">
+            <h1>Os segredos das Landing Pages que convertem muito</h1>
+            <ul>
+                <li>Titúlos simples e diretos que deixem claro a oferta</li>
+                <li>Nada de menus ou links externos na Land Pages</li>
+                <li>Use o poder de vídeos para engajar e provar valor</li>
+            </ul>
+        </div>
+        <!--Container-list---->
+
+            <div class="form">
+                <h2>Preencha o formulário e inicie sua caminhada para o sucesso!</h2>
+
+                <form>
+                    <div class="input-container">
+                        <span>*Nome:</span>
+                        <input type="text" name="nome" required="">
+                        <span>*Email:</span>
+                        <input type="email" name="email" required="">
+                        <span>Telefone:</span>
+                        <input type="tel" name="tel" required="">
+                    </div>
+                    <!-- Input-container --->
+
+                    <p class="warning">*Campos obrigatórios</p>
+
+                    <div class="input-submit-container">
+                        <input type="submit" name="acao" value="Enviar">
+                    </div>
+                    <!-- Input-submit-container --->
+
+                </form>
+                <!-- Fim do Form --->
+
+            </div>
+
+        <!-- Classe Form --->
+        <div class="clear"></div>
+    </div>
+    <!-- Center -->
+
+</div>
+<!--container-banner-->
+<div class="container-2">
+    <div class="center">
+        <h2>Landing Page</h2>
+
+        <p>
+            Landing Page é uma página que possui todos os elementos voltados à conversão do visitante em Lead, oportunidade ou cliente. Também conhecidas como páginas de aterrissagem ou páginas de conversão, as Landing Pages são muito usadas em campanhas de Marketing Digital, pois costumam ter altas taxas de conversão.
+        </p>
+    </div>
+    <!--center-->
+    <div class="arrow"></div>
+</div>
+<!--container-2-->
+
+<div class="container-3">
+    <h2>Venha aprender com quem entende</h2>
+
+    <div class="container-section">
+        <img src="image/icon.png">
+
+        <div class="text-container-single">
+            <h2>Redes sociais</h2>
+
+            <p>Aproveite as redes sociais em que sua empresa está presente para a geração de Leads. Facebook, Twitter, LinkedIn, Instagram… Todas essas redes possuem pessoas que podem se interessar pela sua oferta.
+            </p>
+        </div>
+    </div>
+
+    <div class="container-section">
+        <img src="image/icon.png">
+
+        <div class="text-container-single">
+            <h2>Site/blog</h2>
+
+            <p>Aproveite o tráfego existente e divulgue sua Landing Page nas páginas mais acessadas para gerar Leads e otimizar seu site ou blog para conversão.
+            </p>
+        </div>
+    </div>
+    <div class="container-section">
+        <img src="image/icon.png">
+
+        <div class="text-container-single">
+            <h2>Mídias pagas</h2>
+
+            <p>As mídias pagas podem ajudar muito no impulsionamento do seu negócio. No Google Ads, por exemplo, você encontrará visitantes qualificados, que estão em busca de uma solução ou conteúdo para suas empresas.
+            </p>
+        </div>
+    </div>
+    <!--Container-section-->
+    <div class="clear"></div>
+</div>
+<!--Container-3-->
+</div>
+<!-- Conteiner-banner -->
+<footer>
+    <div class="rodape">
+        &copy; 2019 - Todos os direitos reservados.
+    </div>
+</footer>
+</body>
+</html>
